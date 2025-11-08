98282d308ec1f3f583bce6d8590a8127bf088efb
pradeep kumar <prrajpoot12234@gmail.com>
2025-11-08 15:39:49 +0530

    added backend url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+const allowedOrigins = ['https://prev-year-paper-app-xlxk.vercel.app/', 'http://127.0.0.1:5173'];
 
 app.use(helmet());
 
